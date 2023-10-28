6375fbff2967f1694defb235d47c470415c6ba0f
yusakuvol <yoursakuyu@gmail.com>
2023-10-28 21:07:07 +0900

    feat: 🎸 get query

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,22 @@ import LocationInput from 'ui/components/LocationInput/LocationInput'
 import CurrentWeather from 'ui/weather/CurrentWeather/CurrentWeather'
 import ForecastWeather from 'ui/weather/ForecastWeather/ForecastWeather'
 
-export default function Page() {
+export default function Page({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string | string[] | undefined }
+}) {
+  const defaultLocation = 'tokyo'
+  const location = searchParams.location
+    ? (searchParams.location as string)
+    : defaultLocation
+
   return (
     <main>
       <h1>WeatherApp</h1>
       <LocationInput />
-      <CurrentWeather location="osaka" />
-      <ForecastWeather location="osaka" />
+      <CurrentWeather location={location} />
+      <ForecastWeather location={location} />
     </main>
   )
 }
