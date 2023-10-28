a99b86cf5e5ab997a4366ec518004fb9e49fd861
yusakuvol <yoursakuyu@gmail.com>
2023-10-28 21:06:44 +0900

    feat: 🎸 add server action

--- app/ui/components/LocationInput/LocationInput.tsx
+++ app/ui/components/LocationInput/LocationInput.tsx
@@ -1,8 +1,20 @@
-'use client'
+import { redirect } from 'next/navigation'
+import { z } from 'zod'
 
-export default function LocationInput() {
+export default async function LocationInput() {
+  const handleSubmit = async (data: FormData) => {
+    'use server'
+    const schema = z.object({
+      location: z.string(),
+    })
+    schema.parse({
+      location: data.get('location'),
+    })
+    redirect(`/?location=${data.get('location')}`)
+  }
+
   return (
-    <form action="" method="get">
+    <form action={handleSubmit}>
       <label htmlFor="location">Location</label>
       <input type="text" name="location" />
       <button type="submit">Search</button>
