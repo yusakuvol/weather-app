5c2c79c7444703a42fe691a0b922e6b29f9368bf
yusakuvol <yoursakuyu@gmail.com>
2023-10-28 14:24:10 +0900

    feat: 🎸 add location input component

---
+++ app/ui/components/LocationInput/LocationInput.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+export default function LocationInput() {
+  return (
+    <form action="" method="get">
+      <label htmlFor="location">Location</label>
+      <input type="text" name="location" />
+      <button type="submit">Search</button>
+    </form>
+  )
+}
