bf9d8c0503013f0e59d70f71787bf77ddedf4363
yusakuvol <yoursakuyu@gmail.com>
2023-10-28 14:24:26 +0900

    feat: 🎸 update top page

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,14 @@
+import LocationInput from 'ui/components/LocationInput/LocationInput'
+import CurrentWeather from 'ui/weather/CurrentWeather/CurrentWeather'
+import ForecastWeather from 'ui/weather/ForecastWeather/ForecastWeather'
+
 export default function Page() {
   return (
     <main>
-      <h1>Hello, World!</h1>
-      <p>This is a sample page.</p>
+      <h1>WeatherApp</h1>
+      <LocationInput />
+      <CurrentWeather location="osaka" />
+      <ForecastWeather location="osaka" />
     </main>
   )
 }
