ac5994582aeec3ba696aae35ea7996908ad6372b
yusakuvol <yoursakuyu@gmail.com>
2023-10-28 14:23:35 +0900

    feat: 🎸 add current weather box

---
+++ app/ui/weather/CurrentWeather/CurrentWeather.tsx
@@ -0,0 +1,17 @@
+import { fetchCurrentWeatherByWord } from 'lib/data'
+
+type Props = {
+  location: string
+}
+
+export default async function CurrentWeather({ location }: Props) {
+  const currentWeather = await fetchCurrentWeatherByWord(location)
+
+  return (
+    <div>
+      <h2>Current Weather</h2>
+      <p>{currentWeather.location.name}</p>
+      <p>{currentWeather.current.condition.text}</p>
+    </div>
+  )
+}
