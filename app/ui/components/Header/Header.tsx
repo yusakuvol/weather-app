17f0b46b96aa4ef3146478283a0d60a53176d181
yusakuvol <yoursakuyu@gmail.com>
2023-10-29 16:04:21 +0900

    style: 💄 add image priority

--- app/ui/components/Header/Header.tsx
+++ app/ui/components/Header/Header.tsx
@@ -7,7 +7,13 @@ export default function Header() {
   return (
     <header className={styles?.header}>
       <Link href="/" className={styles?.linkContainer}>
-        <Image src="/logo.svg" alt="logo" width="90" height="90" />
+        <Image
+          src="/logo.svg"
+          alt="logo"
+          width="90"
+          height="90"
+          priority={true}
+        />
         <h1 className={styles?.h1}>WeatherApp</h1>
       </Link>
     </header>
