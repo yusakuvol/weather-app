77ae17593101f0860446db20c31db7890176d64c
yusakuvol <yoursakuyu@gmail.com>
2023-10-29 11:39:00 +0900

    style: 💄 change top styles

---
+++ app/layout.css.ts
@@ -0,0 +1,7 @@
+import { style } from '@vanilla-extract/css'
+
+export const styles = {
+  body: style({
+    margin: 0,
+  }),
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,13 @@
+import { styles } from './layout.css'
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
   return (
-    <html lang="ja">
-      <body>{children}</body>
+    <html lang="en">
+      <body className={styles.body}>{children}</body>
     </html>
   )
 }
