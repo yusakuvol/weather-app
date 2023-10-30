84d38b28497b61856c13fc12d0a97d5a5f749987
yusakuvol <yoursakuyu@gmail.com>
2023-10-31 00:04:24 +0900

    feat: 🎸 add current weather temp

--- app/ui/weather/CurrentWeather/CurrentWeather.tsx
+++ app/ui/weather/CurrentWeather/CurrentWeather.tsx
@@ -1,4 +1,7 @@
+import Image from 'next/image'
+
 import { fetchCurrentWeatherByWord } from '~/app/lib/data'
+import { styles } from '~/app/ui/weather/CurrentWeather/CurrentWeather.css'
 
 type Props = {
   location: string
@@ -7,11 +10,24 @@ type Props = {
 export default async function CurrentWeather({ location }: Props) {
   const currentWeather = await fetchCurrentWeatherByWord(location)
 
+  const iconSplit = currentWeather.current.condition.icon.split('/')
+  const iconSrc = `/weather/${iconSplit[iconSplit.length - 2]}/${
+    iconSplit[iconSplit.length - 1]
+  }`
+
+  console.log(currentWeather.current.temp_c)
+
   return (
-    <div>
-      <h2>Current Weather</h2>
-      <p>{currentWeather.location.name}</p>
-      <p>{currentWeather.current.condition.text}</p>
-    </div>
+    !currentWeather.error && (
+      <article className={styles?.container}>
+        <h2 className={styles?.h2}>Current Weather</h2>
+        <div className={styles?.content}>
+          <p>{currentWeather.location.name}</p>
+          <Image src={iconSrc} width={50} height={50} alt="Weather icon" />
+          <p>{currentWeather.current.condition.text}</p>
+          <p>{currentWeather.current.temp_c}°C</p>
+        </div>
+      </article>
+    )
   )
 }
