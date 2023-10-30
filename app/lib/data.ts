461a667ada44da84d69d05ffc2516ea60e1ac9ef
yusakuvol <yoursakuyu@gmail.com>
2023-10-30 23:10:05 +0900

    feat: 🎸 add error catch

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -1,15 +1,25 @@
 export async function fetchCurrentWeatherByWord(word: string) {
-  const response = await fetch(
-    `${process.env.NEXT_PUBLIC_WEATHER_API_URL}/current.json?key=${process.env.NEXT_PUBLIC_WEATHER_API_TOKEN}&q=${word}`
-  )
-  const data = await response.json()
-  return data
+  try {
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_WEATHER_API_URL}/current.json?key=${process.env.NEXT_PUBLIC_WEATHER_API_TOKEN}&q=${word}`
+    )
+    const data = await response.json()
+    return data
+  } catch (error) {
+    console.error('Error:', error)
+    throw new Error('Error fetching current weather')
+  }
 }
 
 export async function fetchForecastWeatherByWord(word: string) {
-  const response = await fetch(
-    `${process.env.NEXT_PUBLIC_WEATHER_API_URL}/forecast.json?key=${process.env.NEXT_PUBLIC_WEATHER_API_TOKEN}&q=${word}`
-  )
-  const data = await response.json()
-  return data
+  try {
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_WEATHER_API_URL}/forecast.json?key=${process.env.NEXT_PUBLIC_WEATHER_API_TOKEN}&q=${word}`
+    )
+    const data = await response.json()
+    return data
+  } catch (error) {
+    console.error('Error:', error)
+    throw new Error('Error fetching forecast weather')
+  }
 }
