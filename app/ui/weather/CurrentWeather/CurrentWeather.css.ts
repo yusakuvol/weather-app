d4bc7b2455a6a63f960404c6094f0553933bd78e
yusakuvol <yoursakuyu@gmail.com>
2023-10-31 21:17:39 +0900

    feat: 🎸 update currentweather

--- app/ui/weather/CurrentWeather/CurrentWeather.css.ts
+++ app/ui/weather/CurrentWeather/CurrentWeather.css.ts
@@ -2,8 +2,7 @@ import { style } from '@vanilla-extract/css'
 
 export const styles = {
   container: style({
-    marginLeft: '0.75rem',
-    marginRight: '0.75rem',
+    margin: '0.75rem',
   }),
   h2: style({
     fontSize: '1rem',
@@ -13,5 +12,38 @@ export const styles = {
   content: style({
     backgroundColor: 'white',
     borderRadius: '0.5rem',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    padding: '0.75rem',
+  }),
+  locationName: style({
+    margin: 0,
+    textAlign: 'center',
+  }),
+  contentImage: style({
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  }),
+  temperature: style({
+    fontSize: '2rem',
+    fontWeight: 'bold',
+    margin: 0,
+  }),
+  condition: style({
+    margin: 0,
+    textAlign: 'center',
+  }),
+  informationContainer: style({
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingLeft: '0.5rem',
+    paddingRight: '0.5rem',
+  }),
+  hr: style({
+    width: '100%',
+    backgroundColor: '#ccc',
   }),
 }
--- app/ui/weather/CurrentWeather/CurrentWeather.tsx
+++ app/ui/weather/CurrentWeather/CurrentWeather.tsx
@@ -10,22 +10,50 @@ type Props = {
 export default async function CurrentWeather({ location }: Props) {
   const currentWeather = await fetchCurrentWeatherByWord(location)
 
+  if (currentWeather.error) {
+    return (
+      <article className={styles?.container}>
+        <h2 className={styles?.h2}>Current Weather</h2>
+        <p>{currentWeather.error.message}</p>
+      </article>
+    )
+  }
+
   const iconSplit = currentWeather.current.condition.icon.split('/')
   const iconSrc = `/weather/${iconSplit[iconSplit.length - 2]}/${
     iconSplit[iconSplit.length - 1]
   }`
-
-  console.log(currentWeather.current.temp_c)
 
   return (
     !currentWeather.error && (
       <article className={styles?.container}>
         <h2 className={styles?.h2}>Current Weather</h2>
         <div className={styles?.content}>
-          <p>{currentWeather.location.name}</p>
-          <Image src={iconSrc} width={50} height={50} alt="Weather icon" />
-          <p>{currentWeather.current.condition.text}</p>
-          <p>{currentWeather.current.temp_c}°C</p>
+          <p className={styles?.locationName}>{currentWeather.location.name}</p>
+          <div className={styles?.contentImage}>
+            <Image src={iconSrc} width={70} height={70} alt="Weather icon" />
+            <p className={styles?.temperature}>
+              {currentWeather.current.temp_c}°C
+            </p>
+          </div>
+          <p className={styles?.condition}>
+            {currentWeather.current.condition.text}
+          </p>
+          <hr className={styles?.hr} />
+          <div className={styles?.informationContainer}>
+            <span>Wind</span>
+            <span>{currentWeather.current.wind_kph} km/h</span>
+          </div>
+          <hr className={styles?.hr} />
+          <div className={styles?.informationContainer}>
+            <span>Feels like</span>
+            <span>{currentWeather.current.feelslike_c}°C</span>
+          </div>
+          <hr className={styles?.hr} />
+          <div className={styles?.informationContainer}>
+            <span>Humidity</span>
+            <span>{currentWeather.current.humidity}%</span>
+          </div>
         </div>
       </article>
     )
